110a6b723214d4ae96d7a43dc35013dc7340eee1
Anna Sheveleva <anna.girl.v@mail.ru>
2022-04-30 19:59:47 +0300

    st

--- src/stack.js
+++ src/stack.js
@@ -12,16 +12,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
+class Stack {
+  push(/* element */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
 
-const Stack = [];
+  pop() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
 
-
-  stack.push();
-
-  stack.pop();
-
-  stack.peek() = stack[stack.length - 1];
-
+  peek() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+  }
+}
 
 module.exports = {
   Stack
